2f2df193c42a19b94fde4837c21ff011c0753ed9
dcts <thomas@dcts.dev>
2022-12-07 00:06:24 +0100

    :tada: prepare 2022day7

---
+++ 2022/07/input.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
+  .toString()
+  .trim()
+
+module.exports = {
+  input
+}
