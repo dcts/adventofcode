d7833e3a7c29533fde0bc26805671c253f764497
dcts <thomas@dcts.dev>
2022-12-26 16:48:24 +0100

    :sparkle: d21 done

--- 2022/21/run.js
+++ 2022/21/run.js
@@ -1,3 +1,4 @@
+const algebra = require("algebra.js");
 const { inputRaw, process } = require("./input.js");
 
 // recursive function to calculate a nodes value
@@ -18,39 +19,50 @@ function calculate(node, tree) {
   }[node.operator];
 }
 
-// computes difference between left and right side
-// given a value for humn
-function compareSides(tree, humnValue) {
-  tree.humn = { value: humnValue };
-  const leftTree = tree[tree.root.left];
-  const rightTree = tree[tree.root.right];
-  const l = calculate(leftTree, tree);
-  const r = calculate(rightTree, tree);
-  return l - r;
+// returns a string of the needed operations
+// to calculate the result
+function nodeCalculation(node, tree) {
+  if (node.value !== undefined) {
+    return `${node.value}`;
+  }
+  let left = node.left;
+  let right = node.right;
+  if (isNaN(Number(left)) && left !== "humn") {
+    left = nodeCalculation(tree[left], tree);
+  } 
+  if (isNaN(Number(right) && right !== "humn")) {
+    right = nodeCalculation(tree[right], tree);
+  }
+  // left = eval(left) || left;
+  // right = eval(right) || right;
+  return `(${left}${node.operator}${right})`;
 }
 
 // PART1
-const input1 = process(inputRaw);
-const result1 = calculate(input1.root, input1);
-console.log({result1});
+let tree = process(inputRaw);
+const result1 = calculate(tree.root, tree);
 
 // PART2
-// BRUTE FOrCE APProACH Not WORKING...!
-// const input2 = process(inputRaw);
-// let target = 0;
-// let tempRes = compareSides(input2, target);
-// while(tempRes > 0) {
-//   target += 1000;
-//   tempRes = compareSides(input2, target);
-// }
-// const result2 = target;
-// console.log({result2});
-// const result2 = 
+tree = process(inputRaw);
+const leftNode = tree[tree.root.left];
+const rightNode = tree[tree.root.right];
 
+const leftSideStr = nodeCalculation(leftNode, tree);
+const rightSideStr = nodeCalculation(rightNode, tree);
+const leftSide = algebra.parse(leftSideStr);
+const rightSide = algebra.parse(rightSideStr);
+
+const eq = new algebra.Equation(leftSide, rightSide);
+const solution = eq.solveFor("humn");
+const result2 = Math.round(eval(solution.toString())); 
+// needs rounding because otherwise its a wrong solution.
+// this is caused either by
+// (a) the solution is actually technically wrong, because 0.1+0.2=0.300000004 
+// (b) the package algebra.js has a bug, see: https://github.com/nicolewhite/algebra.js/issues/93
 
 console.log("=== AdventOfCode 2022-day21 ===");
 console.log("\npart_one:");
 console.log(result1);
 console.log("\npart_two: ")
-// console.log(result2); // @todo insert result part_two
+console.log(result2);
 console.log("\n\n");
